4ce93946b18dba29e604c6103b1d2d66b177f0c6
Miroslav <pomanovmiroslav@gmail.com>
2023-08-31 15:44:31 +0300

    changes

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  SingleProducts,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+  AuthWrapper,
+} from "./pages";
+
+function App() {
+  return (
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProducts />} />
+          <Route
+            path="/checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
+  );
+}
+
+export default App;
